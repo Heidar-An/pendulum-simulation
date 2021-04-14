f7145968b074edfdf6321244619d45b6240cb527
Heidar-An <74025356+Heidar-An@users.noreply.github.com>
2021-04-14 17:04:11 +0100

    added in a trace line for the pendulum
    Change the values of mass, gravity or damping to change the motion of the pendulum

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ let endPoint;
 let g = 2;
 let angleV = 0;
 let angleA = 0;
-
+let points = []
 function setup() {
   createCanvas(400, 400);
   endPoint = createVector();
@@ -15,6 +15,7 @@ function draw() {
   background(220);
   let lengthC = cos(angle) * straight;
   let lengthS = sin(angle) * straight;
+  
   endPoint.x = lengthS + 200;
   endPoint.y = lengthC;
   angleA = (-g/straight) * sin(angle);
@@ -22,7 +23,17 @@ function draw() {
   angle += angleV;
   //this part is optional, some damping if you want it.
   angleV *= 0.999;
+  trace(endPoint.x, endPoint.y)
   strokeWeight(4)
   line(200, 0, endPoint.x, endPoint.y)
   circle(endPoint.x, endPoint.y, 64)
+  
+}
+//trace line for the pendulum
+function trace(xCor, yCor){
+  strokeWeight(2);
+  points.push([xCor, yCor])
+  for(let i = 0;i < points.length - 1; i++){
+    line(points[i][0], points[i][1], points[i + 1][0], points[i + 1][1])
+  }
 }
